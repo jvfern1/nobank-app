0272d79d1b8dae05122bc58ed306ea61368c9925
SthefanyNovaes <sthefany.novaes13@gmail.com>
2024-04-01 20:14:08 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import styles from './src/Form/style'
+import { TextInput, Button, Image } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View style={styles.form}>
+      <View style={styles.background}>
+          <Text style={styles.nomeusuario}> Olá, Felipe Melo</Text>          
+            <TextInput style={styles.procurar}> Procurar.. </TextInput>
+            <View style={styles.espaco}></View>
+              
+              <View style={styles.cartao}>
+                <Text style={styles.cartaodecredito}>Cartão de Crédito</Text>
+                <Text style={styles.fatura}>Fatura</Text>
+                <Text style={styles.dinheiro}>R$ 1.500,00</Text>
+                <Text style={styles.pagarfatura}> Pagar Fatura </Text>
+              </View>
+                
+                <View style={styles.espaco}></View>
+                  <View style={styles.cartao}>
+                    <Text style={styles.cartaodecredito}>Conta</Text>
+                    <Text style={styles.fatura}>Saldo disponivel</Text>
+                    <Text style={styles.dinheiro}> R$ 300,00 </Text>
+                    <Text style={styles.pagarfatura}> Transferir </Text>
+                  </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                  <View style={styles.espaco}>
+                    <Text style={styles.meuscartoes}>Meus cartões</Text>
+                  </View>
+              
+              
+      
+      </View>
+   
+    </View>
+  )
+}
---
+++ src/Form/style.js
@@ -0,0 +1,81 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#8803BB',
+        padding: 30,
+        height: '100%',
+},
+    nomeusuario: {
+        color: 'white',
+        paddingTop: 90,
+        paddingBottom: 20,
+
+    },
+
+    procurar: {
+        width: 340,
+        backgroundColor: '#FFF',
+        height: 50,
+        padding: 10,
+        borderRadius: 10,
+       
+
+    },
+
+    cartao: {
+        width: 400,
+        backgroundColor: '#FFF',
+        height: 230,
+        borderRadius: 10,
+        
+    
+    },
+
+    espaco: {
+        padding: 10,
+    },
+
+    cartaodecredito: {
+        padding: 10,
+        fontSize: 15,
+
+
+    },
+
+    fatura: {
+        paddingLeft: 10,
+    },
+
+    dinheiro: {
+        color: '#0099CC', 
+        fontSize: 30,
+        paddingLeft: 10,
+        paddingTop: 10,
+    },
+
+    pagarfatura: {
+        color: '#fff',
+        backgroundColor: '#8803BB',
+        height: 30,
+        width: 150,
+        borderRadius: 15,
+        paddingLeft: 30,
+
+    },
+
+    meuscartoes: {
+        color: 'white',
+        backgroundColor: '#0099CC',
+        width: 350,
+        height: 50,
+        borderRadius: 10,
+        textAlign: 'center',
+        justifyContent: 'center',
+        paddingTop: 10,
+    }
+
+})
+
+export default styles 
+
